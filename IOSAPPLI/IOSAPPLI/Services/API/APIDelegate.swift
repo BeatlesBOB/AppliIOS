ae6a9b70ae67873e25c56e9bbe0c45798c4a76c6
Nikolnikov Kirill <k.nikolnikov@apparence.io>
2019-11-26 17:38:15 +0100

    Creation du service qui fera appel à l'API

---
+++ IOSAPPLI/IOSAPPLI/Services/API/APIDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  APIDelegate.swift
+//  IOSAPPLI
+//
+//  Created by Kirill Nikolnikov on 26/11/2019.
+//  Copyright © 2019 Natha Allard. All rights reserved.
+//
+
+import Foundation
--- IOSAPPLI/IOSAPPLI/ViewController.swift
+++ IOSAPPLI/IOSAPPLI/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
+struct Movie: Decodable{
+    let poster_path:String
+    let adult:Bool
+    let overview:String
+    let release_date:String
+    let genre_ids:[Int]
+    let id:Int
+    let original_title:String
+    let original_language:String
+    let title:String
+    let backdrop_path:String
+    let popularity:Double
+    let vote_count:Double
+    let video:Bool
+    let vote_average:Bool
+}
+
 class ViewController: UIViewController {
 
+    //MARK - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
+        let session = URLSession.shared.dataTask(with: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=8d86b8e5fd78601cc8bcf5191cd88788&query=Jack+Reacher")!){
+            (data,response,error) in
+            print(String(data: data!, encoding: .utf8)!)
+        }
+        session.resume()
     }
 
 
