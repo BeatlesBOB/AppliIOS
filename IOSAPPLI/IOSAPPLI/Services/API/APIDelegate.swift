cc90f6655c8a459ba68d2742bed040acb77aa10f
Nikolnikov Kirill <k.nikolnikov@apparence.io>
2019-12-03 09:25:16 +0100

    APIDelegate created / Models / Screens folder

--- IOSAPPLI/IOSAPPLI/Services/API/APIDelegate.swift
+++ IOSAPPLI/IOSAPPLI/Services/API/APIDelegate.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class APIDelegate{
+    var delegate:Delegate?
+    
+    func fetchMovies()->Void{
+        let session = URLSession.shared.dataTask(with: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=8d86b8e5fd78601cc8bcf5191cd88788&query=Jack+Reacher")!)
+        {
+            (data,response,error) in
+            do{
+//                let jsonData = try JSONSerialization.jsonObject(with: data!, options: [])
+                let jsonData = try JSONDecoder().decode(GetResponse.self,from: data!)
+                print(jsonData.results[0].original_title)
+                self.delegate?.callback()
+                
+            }catch{
+                print("Erreur")
+            }
+        }
+        session.resume()
+        
+    }
+}
+
+protocol Delegate {
+    func callback()
+}
