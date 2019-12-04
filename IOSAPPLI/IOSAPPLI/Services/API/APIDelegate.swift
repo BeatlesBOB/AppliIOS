7aba89e9458d1754995e6a20b8e2e4839275cc14
Nikolnikov Kirill <k.nikolnikov@apparence.io>
2019-12-04 09:24:37 +0100

    API calls for Latest Movies / Movie Details / Movie Search

--- IOSAPPLI/IOSAPPLI/Services/API/APIDelegate.swift
+++ IOSAPPLI/IOSAPPLI/Services/API/APIDelegate.swift
@@ -6,30 +6,58 @@
 //  Copyright © 2019 Natha Allard. All rights reserved.
 //
 
+let APIURL = "https://api.themoviedb.org/3/"
+let APIKEY = "api_key=8d86b8e5fd78601cc8bcf5191cd88788"
+
 import Foundation
 
 class APIDelegate{
     var delegate:Delegate?
     
-    func fetchMovies()->Void{
-        let session = URLSession.shared.dataTask(with: URL(string: "https://api.themoviedb.org/3/search/movie?api_key=8d86b8e5fd78601cc8bcf5191cd88788&query=Jack+Reacher")!)
+    func fetchLatestMovies()->Void{
+        let session = URLSession.shared.dataTask(with: URL(string: APIURL+"movie/now_playing?region=fr&"+APIKEY)!)
         {
             (data,response,error) in
             do{
 //                let jsonData = try JSONSerialization.jsonObject(with: data!, options: [])
-                let jsonData = try JSONDecoder().decode(GetResponse.self,from: data!)
-                print(jsonData.results[0].original_title)
-                self.delegate?.callback()
-                
+                let jsonData = try JSONDecoder().decode(LatestMovies.self,from:data!)
+                self.delegate?.callback(movies:jsonData)
             }catch{
-                print("Erreur")
+                print(error)
             }
         }
         session.resume()
-        
+    }
+    
+    func searchMovie(name:String){
+        let session = URLSession.shared.dataTask(with: URL(string:"https://api.themoviedb.org/3/search/movie?query=transformers&"+APIKEY)!)
+            {
+                (data,response,error) in
+                do{
+                    let jsonData = try JSONDecoder().decode(GetResponse.self,from:data!)
+                    self.delegate?.callback(movies: jsonData)
+                }catch{
+                    print(error)
+                }
+            }
+            session.resume()
+    }
+    
+    func getMovieDetails(id:Int){
+        let session = URLSession.shared.dataTask(with: URL(string:APIURL+"movie/"+id.description+"?"+APIKEY)!)
+                {
+                    (data,response,error) in
+                    do{
+                        let jsonData = try JSONDecoder().decode(MovieDetails.self,from:data!)
+                        self.delegate?.callback(movies:jsonData)
+                    }catch{
+                        print(error)
+                    }
+                }
+                session.resume()
     }
 }
 
 protocol Delegate {
-    func callback()
+    func callback(movies:Any)
 }
--- IOSAPPLI/IOSAPPLI/ViewController.swift
+++ IOSAPPLI/IOSAPPLI/ViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,Delegate {
+    func callback(movies: Any) {
+        if(movies is GetResponse){
+            print("yes")
+        }else{
+            print(movies)
+        }
+    }
+    
     
     private let api:APIDelegate = APIDelegate()
 
     //MARK - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        api.delegate = self
+        api.fetchLatestMovies()
+//        api.searchMovie(name: "transformers")
+//        api.getMovieDetails(id: 335988)
     }
 
 
